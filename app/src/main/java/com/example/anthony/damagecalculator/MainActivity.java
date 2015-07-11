737209a4d017c282d339ba310b164e496791f9d9
tw0ng <tw0ng@hotmail.com>
2015-07-11 04:10:04 -0700

    updated MainActivity to start with MainFragment

--- app/src/main/java/com/example/anthony/damagecalculator/MainActivity.java
+++ app/src/main/java/com/example/anthony/damagecalculator/MainActivity.java
@@ -96,6 +96,9 @@ public class MainActivity extends ActionBarActivity {
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
+            if(position == 0) {
+               return MainFragment.newInstance(position + 1);
+            }
             return PlaceholderFragment.newInstance(position + 1);
         }
 
