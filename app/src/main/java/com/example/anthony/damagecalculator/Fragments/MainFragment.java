1aedd426ecfbacd078f116ae442893f432ccbab1
Oliver Phi <istolethisemail.phi@gmail.com>
2015-07-11 04:09:12 -0700

    Testing orbMatch in MainFragment
    orbMatch in MainFragment

--- app/src/main/java/com/example/anthony/damagecalculator/Fragments/MainFragment.java
+++ app/src/main/java/com/example/anthony/damagecalculator/Fragments/MainFragment.java
@@ -4,12 +4,14 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.anthony.damagecalculator.MainActivity;
 import com.example.anthony.damagecalculator.R;
+import com.example.anthony.damagecalculator.Util.DamageCalculationUtil;
 
 /**
  * Created by Thomas on 7/11/2015.
@@ -38,5 +40,6 @@ public class MainFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         //Write your code here
+        Log.d("Testing orbMatch", "orbMatch: " + DamageCalculationUtil.orbMatch(1984, 4, 4, 6, 1));
     }
 }
